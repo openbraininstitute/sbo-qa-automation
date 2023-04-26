ed22e12af75ad0b72f8aa9a23752a85c4103973f
Ayima Okeeva <ayima.okeeva@epfl.ch>
2023-04-26 16:45:10 +0200

    Change build_page.py and test_build.py by adding the login fixture from the conftest.py Add the build_page mark to the pytest.ini

--- tests/test_build.py
+++ tests/test_build.py
@@ -3,9 +3,33 @@ import pytest
 from pages.build_page import BuildPage
 from util.util_base import load_config
 
+class TestBuild:
+    @pytest.mark.build_page
+    def test_build_page(self, setup, login_explore, logger):
+        browser, wait = setup
+        build_page = BuildPage(browser, wait)
+        build_url = build_page.go_to_build_page()
+        assert build_url == 'https://bbp.epfl.ch/mmb-beta/build/load-brain-config'
 
+        # Find "titles on the Build Page"
+        recent_config = build_page.find_recent_configurations()
+        assert recent_config.text == "Recently used configurations"
+        logger.info("Build page main titles are displayed, such as Recent recently used configurations")
+
+        # Check the Release version
+        verify_release_version = build_page.verify_release_version()
+        assert verify_release_version.text == "Release 23.01"
+        logger.info("Build page release version is displayed")
+
+        # Find the default public config and click on it
+        plus_icon_open_default_config = build_page.select_default_config().click()
+        logger.info("Accessing public config link via plus icon")
+
+
+
+
+'''
 @pytest.mark.usefixtures("setup", "logger", "login", "navigate_to_login")
-
 class TestBuild:
     def test_build_page(self, setup, logger, login, navigate_to_login):
         # Navigate to the home page
@@ -37,5 +61,5 @@ class TestBuild:
         # Find the default public config and click on it
         plus_icon_open_default_config = build_page.select_default_config().click()
         logger.info("Accessing public config link via plus icon")
-
+'''
 
