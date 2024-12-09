794a6e9d6d341b0104197e252d5f96570ced0201
Ayima Okeeva <ayima.okeeva@epfl.ch>
2024-12-09 18:33:10 +0100

    Add searching for Isocortex in Model data page Add closing the Brain region panel

--- locators/explore_model_locators.py
+++ locators/explore_model_locators.py
@@ -8,3 +8,10 @@ from selenium.webdriver.common.by import By
 class ExploreModelPageLocators:
     EMODEL_TAB = (By.XPATH, "//li[@title='E-model']")
     ME_MODEL_TAB = (By.XPATH, "//li[@title='ME-model']")
+    CLOSE_BRAIN_REGION_PANEL_BTN = (By.XPATH, "(//button[starts-with(@class, 'ant-btn')]/span["
+                                              "@class='ant-btn-icon'])[1]")
+    BR_SEARCH_REGION_SEARCH_FIELD = (By.XPATH, "//div[@class='ant-select-selector']")
+    BR_SEARCH_FIELD_TYPE = (By.XPATH, "//span[@class='ant-select-selection-search']")
+    BRAIN_REGION_PANEL = (By.XPATH, "(//div[@class='flex h-screen flex-col bg-primary-8'])[1]")
+    SELECTED_BRAIN_REGION = (By.XPATH, "//h1[@title='Isocortex']/span[text()='Isocortex']")
+    SEARCH_REGION = (By.XPATH, "//input[@class='ant-select-selection-search-input']")
--- tests/test_explore_model.py
+++ tests/test_explore_model.py
@@ -5,12 +5,12 @@
 import time
 import os.path
 import pytest
-from selenium.webdriver import Keys
+from selenium.webdriver import Keys, ActionChains
 
+from locators.explore_model_locators import ExploreModelPageLocators
 from locators.explore_page_locators import ExplorePageLocators
 from pages.explore_model_page import ExploreModelDataPage
 from pages.explore_page import ExplorePage
-
 
 current_directory = os.getcwd()
 relative_file_path = 'scraped_links.txt'
@@ -32,4 +32,26 @@ class TestExploreModelPage:
         emodel_tab = explore_model.find_emodel_tab()
         logger.info("E-model data tab is found")
 
+        cerebrum_title = explore_model.find_cerebrum_title()
+        cerebrum_text = cerebrum_title.text
+        logger.info(f"Found text: {cerebrum_text}")
 
+        brain_region_search_field = explore_model.find_brain_region_search_field(timeout=25)
+        assert brain_region_search_field.is_displayed()
+        logger.info("Bran region panel search field is found")
+        brain_region_search_field.send_keys(Keys.ENTER)
+        logger.info("Clicked on the brain region search input field")
+        find_input_file_and_wait = ExploreModelPageLocators.SEARCH_REGION
+        explore_model.wait_for_long_load(find_input_file_and_wait)
+        logger.info("Waiting for page to load")
+
+        brain_region_search_field.send_keys("Isocortex")
+        logger.info("Searching for 'Isocortex'")
+        brain_region_search_field.send_keys(Keys.ENTER)
+
+        brain_region_panel_close_btn = explore_model.brain_region_panel_close_btn()
+        assert brain_region_panel_close_btn.is_displayed(), ("Brain region panel close button is "
+                                                             "found")
+        brain_region_panel_close_btn.click()
+        logger.info("Brain region panel is toggled close")
+        time.sleep(5)
