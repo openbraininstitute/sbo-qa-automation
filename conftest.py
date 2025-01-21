a0eed943f7bf9576d9544ba943a25f9668ed18f8
Ayima Okeeva <ayima.okeeva@openbraininstitute.org>
2025-01-21 11:58:46 +0100

    Check the list of files

--- conftest.py
+++ conftest.py
@@ -16,10 +16,6 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.safari.options import Options as SafariOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
-from selenium.webdriver.safari.service import Service as SafariService
-from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
