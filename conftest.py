c94b00debf196f31e23222aaab1373a0724089ef
Ayima Okeeva <ayima.okeeva@openbraininstitute.org>
2025-01-22 15:01:14 +0100

    Add SafarOptions and taking screeshots during failure for production step

--- conftest.py
+++ conftest.py
@@ -16,6 +16,8 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.safari.options import Options as SafariOptions
+from selenium.webdriver.safari.service import Service as SafariService
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
