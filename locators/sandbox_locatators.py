bd0f1c68019fcdd5c1ce571aa4397fa4d0f5e047
Ayima Okeeva <ayima.okeeva@openbraininstitute.org>
2025-01-28 16:54:02 +0100

    Add verificaion for creating a VL and project in sandbox

--- locators/sandbox_locatators.py
+++ locators/sandbox_locatators.py
@@ -14,8 +14,14 @@ class SandboxPageLocators:
     INPUT_VLAB_EMAIL = (By.CSS_SELECTOR, "input[id='lab_form_email']")
     INPUT_VLAB_ENTITY = (By.CSS_SELECTOR, "input[id='lab_form_entity']")
     MODAL_NEXT_BTN = (By.XPATH, "//button[@type='submit']/span[contains(text(), 'Next')]")
-    # MODAL_NEXT_BTN = (By.XPATH, "//button[@type='submit']")
     CREATE_VL = (By.XPATH, "(//button[@type='submit'])[2]")
+    VL_BANNER_TITLE = (By.XPATH, "//span[@data-testid='lab-detail-banner-name-element' and contains(text(), 'UI Tests Virtual Lab')]")
+    VL_OVERVIEW = (By.XPATH, "//div[@class='mx-4' and text()='Virtual lab overview']")
+    PROJECTS = (By.XPATH, "//span[.='Projects']")
+    CREATE_PROJECT_BTN = (By.XPATH, "(//button[@type='button'])[3]")
+    INPUT_PROJECT_NAME = (By.XPATH, "//input[@id='name']")
+    INPUT_PROJECT_DESCRIPTION = (By.XPATH, "//textarea[@id='description']")
+    SAVE_PROJECT = (By.XPATH, "//button[@title='Save Changes' and @type='submit']")
 
 
 
