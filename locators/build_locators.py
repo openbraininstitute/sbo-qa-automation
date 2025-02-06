cc8e19738671553b737dc1dd6f0ba66dfc16f924
Ayima Okeeva <ayima.okeeva@openbraininstitute.org>
2025-02-06 17:09:12 +0100

    Add first verifications of building a single neuron

--- locators/build_locators.py
+++ locators/build_locators.py
@@ -5,4 +5,12 @@
 from selenium.webdriver.common.by import By
 
 class BuildLocators:
-    BUILD = (By.XPATH, "")
+    BUILD_MENU_TITLE = (By.XPATH, "//div[@class='mx-4' and text()='Build']")
+    NEW_MODEL_TAB = (By.XPATH, "//label[@for='scope-filter-new']")
+    SINGLE_NEURON_TITLE = (By.XPATH, "//div[text()='Single neuron']")
+    BUILD_SINGLE_NEURON_BTN = (By.XPATH, "//button[@type='button' and contains(text(), 'Build')]")
+    FORM_BUILD_NEURON_TITLE = (By.XPATH, "//div[contains(text(), 'Build a new single neuron')]")
+    FORM_NAME = (By.XPATH, "//input[@id='name']")
+    FORM_DESCRIPTION = (By.XPATH, "//textarea[@id='description']")
+    FORM_BRAIN_REGION = (By.XPATH, "//input[@id='brainRegion']")
+    START_BUILDING_BTN = (By.XPATH, "//button[@type='submit']/span[text()='Start building']")
