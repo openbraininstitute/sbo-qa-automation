99f69feeaf91429b47b7e350ad9835cb3d0b8213
Ayima Okeeva <ayima.okeeva@openbraininstitute.org>
2025-02-06 20:07:19 +0100

    Additional test for the first page when creating the single neuron model

--- pages/build.py
+++ pages/build.py
@@ -36,6 +36,15 @@ class Build(HomePage, LinkChecker):
     def form_build_single_neuron_title(self):
         return self.find_element(BuildLocators.FORM_BUILD_NEURON_TITLE)
 
+    def created_by_name(self):
+        return self.find_all_elements(BuildLocators.CREATED_BY_NAME)
+
+    def creation_date(self):
+        return self.find_element(BuildLocators.CREATION_DATE)
+
+    def date(self):
+        return self.find_element(BuildLocators.DATE)
+
     def form_name(self):
         return self.element_visibility(BuildLocators.FORM_NAME)
 
@@ -47,3 +56,25 @@ class Build(HomePage, LinkChecker):
 
     def start_building_btn(self):
         return self.element_to_be_clickable(BuildLocators.START_BUILDING_BTN)
+
+    def sn_name(self):
+        return self.find_element(BuildLocators.SN_NAME)
+
+    def sn_description(self):
+        return self.find_element(BuildLocators.SN_DESCRIPTION)
+
+    def sn_creation_date(self):
+        return self.find_element(BuildLocators.SN_CREATION_DATE)
+
+    def sn_created_by(self):
+        return self.find_all_elements(BuildLocators.SN_CREATED_BY)
+
+    def sn_brain_region(self):
+        return self.find_element(BuildLocators.SN_BRAIN_REGION)
+
+    def sn_mtype(self):
+        return self.find_element(BuildLocators.SN_MTYPE)
+
+    def sn_etype(self):
+        return self.find_element(BuildLocators.SN_ETYPE)
+
