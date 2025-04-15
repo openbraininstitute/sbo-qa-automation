af5b278304201b63714958b8aff37b7690f66cda
Ayima <ayima.okeeva@openbraininstitute.org>
2025-04-15 10:09:29 +0200

    Test searching for specific M-model and E-model. Build the ME-model using these records Save the ME-model.

--- locators/build_locators.py
+++ locators/build_locators.py
@@ -5,6 +5,7 @@
 from selenium.webdriver.common.by import By
 
 class BuildLocators:
+    BRAIN_REGION_PANEL_TOGGLE = (By.XPATH, "(//span[@aria-label='minus'])[1]")
     BUILD_MENU_TITLE = (By.XPATH, "//div[@class='mx-4' and text()='Build']")
     NEW_MODEL_TAB = (By.XPATH, "//label[@for='scope-filter-new']")
     SINGLE_NEURON_TITLE = (By.XPATH, "//div[text()='Single neuron']")
@@ -18,20 +19,20 @@ class BuildLocators:
     CREATION_DATE = (By.XPATH, "//div[contains(text(), 'Creation Date')]")
     DATE = (By.XPATH, "//div[contains(text(), 'Creation Date')]/following-sibling::div")
     SN_NAME = (By.XPATH, "//div[contains(@class, 'text-neutral-4') and text()='name']/following-sibling::div/span")
-    SN_DESCRIPTION = (By.XPATH, "//div[contains(@class, 'text-neutral-4') and text()='description']/following-sibling::div")
+    SN_DESCRIPTION = (By.XPATH, "//div[contains(@class, 'text-neutral-4') and text()='description']")
     SN_CREATED_BY = (By.XPATH, "//div[contains(@class, 'text-neutral-4') and text()='created date']/following-sibling::div")
     SN_CREATION_DATE = (By.XPATH, "//div[contains(@class, 'text-neutral-4') and text()='created by']/following-sibling::div/ul/li")
     SN_BRAIN_REGION = (By.XPATH, "//div[contains(@class, 'text-neutral-4') and text()='brain region']/following-sibling::div")
     SN_MTYPE = (By.XPATH, "//div[contains(@class, 'text-neutral-4') and text()='m-type']/following-sibling::div")
     SN_ETYPE = (By.XPATH, "//div[contains(@class, 'text-neutral-4') and text()='e-type']/following-sibling::div")
-    SELECT_M_MODEL = (By.XPATH, "(//button[normalize-space()='Select m-model'])[1]")
-    SELECT_E_MODEL = (By.XPATH, "(//button[normalize-space()='Select e-model'])[1]")
+    SELECT_M_MODEL_BTN = (By.XPATH, "(//button[normalize-space()='Select m-model'])[1]")
+    SELECT_E_MODEL_BTN = (By.XPATH, "(//button[normalize-space()='Select e-model'])[1]")
     SEARCH_INPUT_FIELD = (By.XPATH, "//input[@placeholder='Search for resources...']")
-    SEARCH_NAME = (By.XPATH, "(//td[@title='C060114A5'])[1]")
-    SEARCHED_RECORD = (By.XPATH, "//tr[@data-row-key='https://openbluebrain.com/api/nexus/v1/resources/bbp/mmb-point"
-                                 "-neuron-framework-model/_/https:%2F%2Fbbp.epfl.ch%2Fdata%2Fbbp%2Fmmb-point-neuron"
-                                 "-framework-model%2F8325c79c-d0ff-4de6-aac5-a703a85785c9']")
-    TICK_SEARCHED_RECORD = (By.XPATH, "(//span[@class='ant-radio ant-wave-target'])[1]")
+    SEARCH_M_MODEL_NAME = (By.XPATH, "(//td[@title='C060114A5'])[1]")
+    SEARCHED_M_RECORD = (By.XPATH, "(//td[@title='C060114A5'][normalize-space()='C060114A5'])[1]")
+    SEARCHED_E_RECORD = (By.XPATH, "//td[@title='EM__1372346__cADpyr__13']")
+    TICK_SEARCHED_M_RECORD = (By.XPATH, "(//span[@class='ant-radio-inner'])[1]")
+    TICK_SEARCHED_E_RECORD = (By.XPATH, "(//span[@class='ant-radio-inner'])[2]")
 
     SAVE = (By.XPATH, "//button[text()='Save']")
 
