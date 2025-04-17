65f0efe75bde74151ba1a57d5f9f32273723e899
Ayima <ayima.okeeva@openbraininstitute.org>
2025-04-17 15:52:18 +0200

    Remove 'use_landing_url' logic

--- conftest.py
+++ conftest.py
@@ -158,17 +158,7 @@ def navigate_to_login(setup, logger, request, test_config):
     """Fixture that navigates to the login page"""
     browser, wait, base_url, lab_id, project_id = setup
     login_page = LoginPage(browser, wait, base_url, logger)
-
-    if request.node.get_closest_marker("use_landing_page"):
-        landing_page = LandingPage(browser, wait, base_url, test_config["landing_url"], logger)
-        landing_page.go_to_landing_page()
-        print(f"INFO: Landing page visited, current URL: {browser.current_url}")
-        landing_page.click_go_to_lab()
-        print(f"INFO: After clicking go to lab, current URL: {browser.current_url}")
-    else:
-        browser.get(f"{base_url}")
-        print(f"INFO: Direct access to base URL: {base_url}")
-    print(f"INFO: Waiting for OpenID login redirect, current URL: {browser.current_url}")
+    browser.get(f"{base_url}")
 
     login_page.wait_for_condition(
         lambda driver: "openid-connect" in driver.current_url,
